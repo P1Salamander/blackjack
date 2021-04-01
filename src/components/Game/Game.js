39ad14b83f0bb83bc4ae48fc7191bffc89d54693
P1Salamander <VasilVasilev@mail.bg>
2021-04-02 01:58:00 +0300

    ops

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./Game.css";
 import * as gameService from "../../services/gameService";
 import { Button } from "primereact/button";
@@ -7,14 +7,6 @@ import { Chip } from "primereact/chip";
 import { Dialog } from "primereact/dialog";
 
 import * as userService from "../../services/userService";
-
-//TODO card value to game value
-//TODO ace 1 or 11
-//TODO draw the cards
-//TODO bet starting and bet ended
-//TODO get user and uptade user balances
-//TODO hit button , stand and doubledown
-//TODO if we have 21, WIN! / ( if we and dealer have 21 ??!?!)
 
 export default function Game(props) {
   const [userCards, setUserCards] = useState([]);
@@ -31,10 +23,10 @@ export default function Game(props) {
 
   const [displayResult, setDisplayResult] = useState(false);
 
-  const [deckId, setDeckId] = useState(null);
+  const [deck, setDeck] = useState({ deckId: null, totalCards: 0, cards: [] });
 
   useEffect(() => {
-    getDeckId();
+    getDeck();
 
     if (!dealerPlaying && !userIsBusted) {
       if (userCardsValue > 21) {
@@ -45,10 +37,9 @@ export default function Game(props) {
       if (userCardsValue === 21) {
         setDealerIsBusted(true);
         setDisplayResult(true);
-        userService.updateBalance(
-          props.user,
-          props.user.balance + betValue * 2
-        );
+        const newBalance = props.balance + betValue * 2;
+        userService.updateBalance(props.user, newBalance);
+        props.updateBalance(newBalance);
       }
 
       return;
@@ -57,16 +48,17 @@ export default function Game(props) {
     if (!dealerPlaying || userIsBusted || dealerIsBusted) return;
 
     if (dealerCardsValue < 21) {
-      getCards(1).then((res) => {
-        const card = getCard(res.cards[0]);
-        dealToDealer(false, card);
-      });
+      setTimeout(() => {
+        dealToDealer(false, getCard());
+      }, 500);
     }
 
     if (dealerCardsValue > 21) {
       setDealerIsBusted(true);
       setDisplayResult(true);
-      userService.updateBalance(props.user, props.user.balance + betValue * 2);
+      const newBalance = props.balance + betValue * 2;
+      userService.updateBalance(props.user, newBalance);
+      props.updateBalance(newBalance);
       return;
     }
 
@@ -75,54 +67,58 @@ export default function Game(props) {
       setDisplayResult(true);
       return;
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dealerCardsValue, userCardsValue]);
 
-  const getDeckId = async () => {
-    if (deckId != null) return;
-    const rawResponse = await gameService.getDeck();
+  const getDeck = async () => {
+    if (deck.deckId != null) return;
+    const rawDeckResponse = await gameService.getDeck();
 
-    const response = await rawResponse.json();
-    setDeckId(response.deck_id);
+    const deckResponse = await rawDeckResponse.json();
+
+    const deckId = deckResponse.deck_id;
+    const totalCards = deckResponse.remaining;
+
+    const rawCardsResponse = await gameService.getCards(deckId, totalCards);
+    const cardsResponse = await rawCardsResponse.json();
+
+    const deckOfCards = [];
+    cardsResponse.cards.forEach((card) => {
+      deckOfCards.push({
+        image: card.image,
+        alt: card.code,
+        value: getValue(card.value),
+      });
+    });
+
+    setDeck({
+      deckId: deckResponse.deck_id,
+      totalCards: deckResponse.remaining,
+      cards: deckOfCards,
+    });
   };
 
-  const getCards = async (numberOfCards) => {
-    const rawResponse = await gameService.getCards(deckId, numberOfCards);
-
-    const response = await rawResponse.json();
-    return response;
-  };
-
-  const getCard = (raw) => {
-    return {
-      image: raw.image,
-      alt: raw.code,
-      value: getValue(raw.value),
-    };
+  const getCard = () => {
+    return deck.cards.pop();
   };
 
   const submitBet = async (e) => {
     e.preventDefault();
     setBetSubmited(true);
 
-    userService.updateBalance(props.user, props.user.balance - betValue);
+    const newBalance = props.balance - betValue;
+    userService.updateBalance(props.user, newBalance);
+    props.updateBalance(newBalance);
 
-    const response = await getCards(3);
-
-    response.cards.forEach((res, index) => {
-      const card = getCard(res);
-      if (index % 2 === 0) {
-        dealToUser(card);
-      } else {
-        dealToDealer(false, card);
-      }
-    });
-
+    dealToUser(getCard());
+    dealToDealer(false, getCard());
+    dealToUser(getCard());
     dealToDealer(true);
   };
 
   const increaseBet = (e) => {
     e.preventDefault();
-    if (betValue + 10 <= props.user.balance) {
+    if (betValue + 10 <= props.balance) {
       setBetValue(betValue + 10);
     }
   };
@@ -136,21 +132,18 @@ export default function Game(props) {
 
   const hit = async (e) => {
     e.preventDefault();
-    const response = await getCards(1);
-    const card = getCard(response.cards[0]);
-    dealToUser(card);
+    dealToUser(getCard());
   };
 
   const doubleDown = async (e) => {
     e.preventDefault();
 
-    userService.updateBalance(props.user, props.user.balance - betValue);
-
+    const newBalance = props.balance - betValue;
+    userService.updateBalance(props.user, props.balance - betValue);
+    props.updateBalance(newBalance);
     setBetValue(betValue * 2);
 
-    const response = await getCards(1);
-    const card = getCard(response.cards[0]);
-    dealToUser(card);
+    dealToUser(getCard());
 
     stand(e);
   };
@@ -162,10 +155,7 @@ export default function Game(props) {
     // removing the back card
     const newDealerCards = dealerCards[0];
     setDealerCards([newDealerCards]);
-
-    const response = await getCards(1);
-    const card = getCard(response.cards[0]);
-    dealToDealer(false, card);
+    dealToDealer(false, getCard());
   };
 
   const dealToUser = async (card) => {
@@ -268,6 +258,12 @@ export default function Game(props) {
               icon="pi pi-minus"
               className="p-button-rounded p-button-danger"
               disabled={betSubmited || betValue - 10 <= 0}
+              tooltip="Decrease Bet"
+              tooltipOptions={{
+                position: "bottom",
+                mouseTrack: true,
+                mouseTrackTop: 15,
+              }}
               onClick={(e) => {
                 decreaseBet(e);
               }}
@@ -276,7 +272,13 @@ export default function Game(props) {
             <Button
               icon="pi pi-plus"
               className="p-button-rounded p-button-help"
-              disabled={betSubmited || betValue + 10 > props.user.balance}
+              disabled={betSubmited || betValue + 10 > props.balance}
+              tooltip="Increase Bet"
+              tooltipOptions={{
+                position: "left",
+                mouseTrack: true,
+                mouseTrackTop: 15,
+              }}
               onClick={(e) => {
                 increaseBet(e);
               }}
@@ -289,6 +291,12 @@ export default function Game(props) {
                 icon="pi pi-angle-up"
                 className="p-button-rounded p-button-success"
                 disabled={dealerPlaying || userIsBusted}
+                tooltip="Hit"
+                tooltipOptions={{
+                  position: "bottom",
+                  mouseTrack: true,
+                  mouseTrackTop: 15,
+                }}
                 onClick={(e) => {
                   hit(e);
                 }}
@@ -297,7 +305,15 @@ export default function Game(props) {
               <Button
                 icon="pi pi-angle-double-up"
                 className="p-button-rounded p-button-warning"
-                disabled={dealerPlaying || userIsBusted}
+                disabled={
+                  dealerPlaying || userIsBusted || props.balance < betValue * 2
+                }
+                tooltip="Double Down"
+                tooltipOptions={{
+                  position: "bottom",
+                  mouseTrack: true,
+                  mouseTrackTop: 15,
+                }}
                 onClick={(e) => {
                   doubleDown(e);
                 }}
@@ -307,6 +323,12 @@ export default function Game(props) {
                 icon="pi pi-minus"
                 className="p-button-rounded p-button-danger"
                 disabled={dealerPlaying || userIsBusted}
+                tooltip="Stand"
+                tooltipOptions={{
+                  position: "left",
+                  mouseTrack: true,
+                  mouseTrackTop: 15,
+                }}
                 onClick={(e) => {
                   stand(e);
                 }}
@@ -317,7 +339,13 @@ export default function Game(props) {
               <Button
                 icon="pi pi-check"
                 className="p-button-rounded p-button-success"
-                disabled={betSubmited}
+                disabled={betSubmited || props.balance < betValue}
+                tooltip="Submit Bet"
+                tooltipOptions={{
+                  position: "bottom",
+                  mouseTrack: true,
+                  mouseTrackTop: 15,
+                }}
                 onClick={(e) => {
                   submitBet(e);
                 }}
@@ -330,13 +358,13 @@ export default function Game(props) {
       <Dialog
         header="Result"
         visible={displayResult}
-        position={"top"}
+        position={"center"}
         onHide={() => reset()}
         modal
-        style={{ width: "50vw" }}
+        style={{ width: "30vw" }}
         footer={renderResultFooter}
-        draggable={false}
-        resizable={false}
+        draggable={true}
+        resizable={true}
         baseZIndex={1000}
       >
         <p className="p-m-0">
